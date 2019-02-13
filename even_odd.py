f1225f910ffda7ca25e679ab1cf5f41c20a5da8c
Melarivas <47441500+Melarivas@users.noreply.github.com>
2019-02-13 09:46:45 -0600

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a whole number"))
+whole_number = number % 2
+
+if input == 0:
+    print("Even")
+elif number == 1:
+    print("Odd")
+else:
+    print("I said a whole number")
